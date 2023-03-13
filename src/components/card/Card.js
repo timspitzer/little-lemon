5829db3d4abe0aef950c85fd3e9fe0c332de4cf4
Tim Spitzer <tim.spitzer10@gmail.com>
2023-03-13 13:55:47 +0100

    special section done

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,14 +2,16 @@ import React from "react";
 
 export default function Card({ title, price, description, img, id }) {
   return (
-    <article key={id}>
+    <article className="card" key={id}>
       <img src={img} alt="dish"></img>
-      <div>
-        <h3>{title}</h3>
-        <span>${price}</span>
+      <div className="text">
+        <div className="header">
+          <h3>{title}</h3>
+          <span>${price}</span>
+        </div>
+        <p>{description}</p>
+        <button>Order a delivery </button>
       </div>
-      <p>{description}</p>
-      <button>Order a delivery</button>
     </article>
   );
 }
--- src/components/highlights-section/HighlightsSection.js
+++ src/components/highlights-section/HighlightsSection.js
@@ -4,20 +4,23 @@ import Card from "../card/Card.js";
 const specials = [
   {
     name: "Greek Salad",
-    description: "Lorem ipsum",
-    price: 0,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quas id, officiis ducimus soluta quos tenetur velit consequatur nulla possimus culpa, sed quibusdam reiciendis repellat aperiam iste dicta iure ratione.",
+    price: 12.99,
     img: "/icons_assets/greek salad.jpg",
   },
   {
     name: "Bruchetta",
-    description: "Lorem ipsum",
-    price: 0,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quas id, officiis ducimus soluta quos tenetur velit consequatur nulla possimus culpa, sed quibusdam reiciendis repellat aperiam iste dicta iure ratione.",
+    price: 5.99,
     img: "/icons_assets/bruchetta.svg",
   },
   {
-    name: "Greek Salad",
-    description: "Lorem ipsum",
-    price: 0,
+    name: "Lemon Dessert",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quas id, officiis ducimus soluta quos tenetur velit consequatur nulla possimus culpa, sed quibusdam reiciendis repellat aperiam iste dicta iure ratione.",
+    price: 5.0,
     img: "/icons_assets/lemon dessert.jpg",
   },
 ];
@@ -30,17 +33,21 @@ const specialsWithId = specials.map((special) => ({
 export default function HighlightsSection() {
   return (
     <section className="highlights-section">
-      <h1>Specials</h1>
-      <button>Online Menu</button>
-      {specialsWithId.map(({ name, price, description, img, id }) => (
-        <Card
-          key={id}
-          title={name}
-          price={price}
-          description={description}
-          img={img}
-        ></Card>
-      ))}
+      <div className="highlights-header">
+        <h1 className="display-title">This weeks specials!</h1>
+        <button>Online Menu</button>
+      </div>
+      <div className="cards">
+        {specialsWithId.map(({ name, price, description, img, id }) => (
+          <Card
+            key={id}
+            title={name}
+            price={price}
+            description={description}
+            img={img}
+          ></Card>
+        ))}
+      </div>
     </section>
   );
 }
